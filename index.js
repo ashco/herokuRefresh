406bc83469c8ff5def6ff0a4970ebf4c362b5874
Ashton <ashchristie@gmail.com>
2018-11-30 18:16:43 -0800

    Better logging

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const https = require('https');
+const format = require('date-fns/format');
 
 var hosts = [
   'https://dream-state-app.herokuapp.com/',
   'https://re-crypt.herokuapp.com/',
 ];
-var interval = 1000 * 60 * 60 * 24;
+var interval = 1000 * 60 * 60; // 1 hour
 
 setInterval(() => {
   hosts.forEach(host => {
-    https.get(host);
-    console.log(`Contacting ${host}`);
+    https.get(host, res => {
+      console.log(
+        `[${format(Date.now(), 'YY-MM-DD HH:MM:SS')}] Contacted ${host}`
+      );
+    });
   });
 }, interval);
